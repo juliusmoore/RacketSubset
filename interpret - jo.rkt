61195eac6450353b41d2717b37ee4b586b9ddc84
JustinOnof <48653801+JustinOnof@users.noreply.github.com>
2019-03-17 16:34:59 -0600

    Update interpret - jo.rkt

--- interpret - jo.rkt
+++ interpret - jo.rkt
@@ -114,12 +114,12 @@
 
 ;Only implement the plain lambda syntax without support
 ;for keyword or optional arguments. Assume there is only one expression in the body.
-;((lambda{first} (args) {second} ---body---- {rest(three)}){car} {cdr})
+; rational ((lambda{first} (args) {second} ---body---- {rest or three}){car} {cdr})
 (define (eval_lambda x state)
 (if (and(and(pair? car(second(x))) (pair? car(third(x)))) (pair? cdr(x))) 
-    ((lambda (execute(car(second(x))), state)  (execute(car(third(x)), state)))
+    ((lambda (execute(second(car(x))), state)  (execute(third(car(x)), state)))
      (execute((cdr(x)) state)))
-(error "Justin's lambda function broke again.")))
+(error "Justin's lambda function broke again. what an idiot.")))
 
 (define (eval_let x state) "empty")
 
