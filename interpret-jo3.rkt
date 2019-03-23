66947e7fefa3019e7d4d5c56529116681f5c3217
JustinOnof <48653801+JustinOnof@users.noreply.github.com>
2019-03-22 20:05:29 -0600

    Update interpret-jo3.rkt

--- interpret-jo3.rkt
+++ interpret-jo3.rkt
@@ -271,13 +271,15 @@
             [c (cadr x)]) ; car cdr  value of first param
        ( execute (append (list (first(third(car x)))) (list (second(third(car x)))) (list c)) state))
       ;else this is the more complicated version
-      (let([a (car(second(car x)))] ; first param
+      (let ;length 2 algorithm 
+          ([a (car(second(car x)))] ; first param
            [b (car(cdr(second(car x))))] ; second param
            [c (caadr x)] ;car car cdr  value of first param
            [d (cadadr x)]) ;car cdr car cdr ; value of second param
         (if(equal? (list (second(third(car x)))) c) ; if c is the first param, run 1, else run 2
-           (execute (append (list (first(third(car x)))) (list c) (list d)) state) ;1
-           (execute (append (list (first(third(car x)))) (list d) (list c)) state))))) ;2 
+           (execute (append (list (first(third(car x)))) (list d) (list c)) state) ;1 
+           (execute (append (list (first(third(car x)))) (list c) (list d)) state))))) ;2
+           
   ;( execute (append (list (first(third(car x)))) (list (second(third(car x)))) (list c)) state))
 
 (define (addToState vars state)
